91e2ffee68b46a6181aaf243c17ba9a44bef9d4d
Harsh Dagar <amandagar000@gmail.com>
2023-11-27 12:49:47 +0530

    added files

---
+++ src/Components/Call/index.jsx
@@ -0,0 +1,11 @@
+import "./caller.sass";
+
+const Caller = () => {
+  return (
+    <div className="chit-chat-call">
+      <div className="chit-chat-call__container"></div>
+    </div>
+  );
+};
+
+export default Caller;
